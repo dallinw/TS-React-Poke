c40559743acf67143c500f75c8c4c2b654c45413
williamsdj6 <dallin.j.williams@leidos.com>
2019-08-27 13:23:32 -0400

    oops i committed a merge conflict :-)

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,6 @@ import TopBar from './components/TopBar';
 import * as pokeList from './consts/Utils'
 import { MuiThemeProvider, Grid } from '@material-ui/core';
 import theme from './theme'
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 3c10a50409f265f01935f7d1344841db069e0ef0
 const generatePost = () => {
   let poke:number = pokeList.getRandomInt(1, 809);                              // pick a random pokemon
   let url:string = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";  // pull down its image from pokemon.com
@@ -23,7 +18,6 @@ const generatePost = () => {
   let post = {"key":key, "src":"https://i.imgur.com/vjLcuFJ.jpg", "username":"The Very Best", "at":"AshKetchum", "message":msg ,"imgLink":str, "color":color};
   return post;
 }
-
 const App: FunctionComponent = () => {
   const [posts, setPosts] = useState([])
   const addPost = () => {
@@ -31,38 +25,33 @@ const App: FunctionComponent = () => {
     let newPosts = [...posts, post]
     setPosts(newPosts)
   }
-  const postList = posts.length ? (
-    posts.map(post => {
-      return(
-              <SocialCard 
-                key={post.key}
-                src={post.src} 
-                username={post.username} 
-                at={post.at} 
-                message={post.message} 
-                imgLink={post.imgLink}
-                color={post.color}
-              />
-            )
-        })
-    ) : (
-      <p className="ctr">No Pokemon have been discovered yet!</p>
-    )
-    return (
-      <MuiThemeProvider theme={theme}>
-        <Grid container justify="center">
-          <ul>
-            <TopBar />
-            {postList}
-            <AddPost addFunc={addPost}/>
-          </ul>
-        </Grid>
-      </MuiThemeProvider>
+    const postList = posts.length ? (
+      posts.map(post => {
+        return(
+                <SocialCard 
+                  key={post.key}
+                  src={post.src} 
+                  username={post.username} 
+                  at={post.at} 
+                  message={post.message} 
+                  imgLink={post.imgLink}
+                  color={post.color}
+                />
+              )
+          })
+      ) : (
+          <p className="ctr">No Pokemon have been discovered yet!</p>
+      )
+      return (
+        <MuiThemeProvider theme={theme}>
+          <Grid container justify="center">
+            <ul>
+              <TopBar />
+              {postList}
+              <AddPost addFunc={addPost}/>
+            </ul>
+          </Grid>
+        </MuiThemeProvider>
     )
 }
-<<<<<<< HEAD
 export default App;
-=======
-
-export default App;
->>>>>>> 3c10a50409f265f01935f7d1344841db069e0ef0
